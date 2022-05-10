c844d5f349ed5917fb53e4294662616d3884c80f
rimolch <67545874+rimolch@users.noreply.github.com>
2022-05-10 12:21:57 +0600

    Create dtb.cpp

---
+++ Number_Theory/Bit_Manipulation/decimal_to_binary/dtb.cpp
@@ -0,0 +1,22 @@
+/* (n>>i) tends to n/2^i//right shift
+   (n<<i) tends to n*2^i//left shift
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void printBinary(int n)
+{
+    for(int i=10;i>=0;i--)// i = 10 (max range decimal number)
+    {
+        cout<<((n>>i)&1);
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    printBinary(n);
+    return 0;
+}
+© 2022 GitHub, Inc.
