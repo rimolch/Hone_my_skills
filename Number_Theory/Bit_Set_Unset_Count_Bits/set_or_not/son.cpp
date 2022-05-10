7890d0b6d4794e0819aa7e0e888457a13fc5be68
rimolch <67545874+rimolch@users.noreply.github.com>
2022-05-10 12:01:14 +0600

    Create son.cpp

---
+++ Number_Theory/Bit_Set_Unset_Count_Bits/set_or_not/son.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printBinary(int n)
+{
+    for(int i=10;i>=0;i--)
+    {
+        cout<<((n>>i)&1);
+    }
+    cout<<endl;
+}
+int main()
+{
+    int a = 9;//1 0 0 1
+    int is_ith_set_or_not = 0;//first bit from left to right which is 1
+    if((a&(1<<is_ith_set_or_not))!=0)//9&1==1
+    {
+        cout<<"SET BIT"<<endl;//ok
+    }
+    else
+    {
+        cout<<"NOT SET BIT"<<endl;
+    }
+    return 0;
+}
