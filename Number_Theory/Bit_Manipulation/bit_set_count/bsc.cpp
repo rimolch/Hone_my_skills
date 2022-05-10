fa405b12a37d83c913bed475c76017b97df8005f
rimolch <67545874+rimolch@users.noreply.github.com>
2022-05-10 12:50:58 +0600

    Create bsc.cpp

---
+++ Number_Theory/Bit_Manipulation/bit_set_count/bsc.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printBinary(int n)
+{
+    for(int i=10;i>=0;i--)
+    {
+        cout<<((n>>i)&1);
+    }
+    cout<<endl;
+}
+int main()
+{
+    int a=9;
+    int c=0;
+    for(int i=31;i>=0;i--)//using 32 bit
+    {
+        if((a&(1<<i))!=0)
+        {
+            c++;
+        }
+    }
+    printBinary(a);
+    cout<<c<<endl;
+    return 0;
+}
