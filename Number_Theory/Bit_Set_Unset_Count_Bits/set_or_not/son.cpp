aca1f827398543134fc742670893933f501a799e
rimolch <67545874+rimolch@users.noreply.github.com>
2022-05-10 12:01:44 +0600

    Update son.cpp

--- Number_Theory/Bit_Set_Unset_Count_Bits/set_or_not/son.cpp
+++ Number_Theory/Bit_Set_Unset_Count_Bits/set_or_not/son.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-void printBinary(int n)
+/*void printBinary(int n)
 {
     for(int i=10;i>=0;i--)
     {
         cout<<((n>>i)&1);
     }
     cout<<endl;
-}
+}*/
 int main()
 {
     int a = 9;//1 0 0 1
