fdba0e3ac5a202521de0615fc8d596a24b5c9187
rimolch <67545874+rimolch@users.noreply.github.com>
2022-05-10 12:57:22 +0600

    Update bsc.cpp

--- Number_Theory/Bit_Manipulation/bit_set_count/bsc.cpp
+++ Number_Theory/Bit_Manipulation/bit_set_count/bsc.cpp
@@ -23,3 +23,22 @@ int main()
     cout<<c<<endl;
     return 0;
 }
+//2nd method using __builtin_popcount() 
+#include<bits/stdc++.h>
+using namespace std;
+void printBinary(int n)
+{
+    for(int i=10;i>=0;i--)
+    {
+        cout<<((n>>i)&1);
+    }
+    cout<<endl;
+}
+int main()
+{
+    int a=9;
+    int c=0;
+    printBinary(a);
+    cout<<__builtin_popcount(a)<<endl;
+    return 0;
+}
